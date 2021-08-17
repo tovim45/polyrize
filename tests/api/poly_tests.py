3ed4aa7fac50f64d1525cc6589c84553a71920a9
giora tovim <giorat@e-bitbox.com>
2021-08-18 00:17:41 +0300

    update test

--- tests/api/poly_tests.py
+++ tests/api/poly_tests.py
@@ -10,6 +10,13 @@ def set_up():
     print("set_up-fixture")
 
 
+def test_created_object():
+    print("Test created object read data from json file data_json_body.py")
+    for data in json_body:
+        print(data)
+        assert created_object(data) == 200
+
+
 def test_get_list_of_poly_data(set_up):
     print("Test get list of all poly data")
     print(get_list_of_poly_data())
@@ -19,18 +26,6 @@ def test_get_list_of_poly_data(set_up):
 def test_get_poly_data(set_up, object_id):
     print("Test get poly data by object_id")
     print(get_poly_data(object_id))
-
-
-def test_delete_poly_dataa(set_up):
-    print("Test delete poly data by object_id")
-    print(delete_poly_data(2))
-
-
-def test_created_object():
-    print("Test created object read data from json file data_json_body.py")
-    for data in json_body:
-        print(data)
-        assert created_object(data) == 200
 
 
 def test_created_object_empty_body():
@@ -48,3 +43,8 @@ def test_get_token_negative_test_empty_json():
 def test_get_token_negative_test_wrong_url():
     print("Test get_token_negative_test wrong url response code = 500")
     assert get_token_negative_test("http://localhost:8000/api", auth_json) == 500
+
+
+def test_delete_poly_dataa(set_up):
+    print("Test delete poly data by object_id")
+    print(delete_poly_data(2))
